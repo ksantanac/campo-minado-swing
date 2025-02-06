d7580139b9fc8e22922e2c8560f558c0b795c426
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-02-06 20:18:19 -0300

    Interface gráfica campo-minado 01.

---
+++ src/br/com/cod3r/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,12 @@
+package br.com.cod3r.cm.visao;
+
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+import javax.swing.*;
+
+public class PainelTabuleiro extends JPanel {
+
+    public PainelTabuleiro(Tabuleiro tabuleiro) {
+
+    }
+}
