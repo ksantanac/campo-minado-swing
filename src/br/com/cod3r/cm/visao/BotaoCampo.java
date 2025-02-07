32ec26a3f266b27f7fcf02afd659eaa88e4cef61
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-02-06 22:02:18 -0300

    Interface gráfica campo-minado 03.

---
+++ src/br/com/cod3r/cm/visao/BotaoCampo.java
@@ -0,0 +1,12 @@
+package br.com.cod3r.cm.visao;
+
+import br.com.cod3r.cm.modelo.Campo;
+
+import javax.swing.*;
+
+public class BotaoCampo extends JButton {
+
+    public BotaoCampo(Campo campo) {
+
+    }
+}
--- src/br/com/cod3r/cm/visao/PainelTabuleiro.java
+++ src/br/com/cod3r/cm/visao/PainelTabuleiro.java
@@ -3,10 +3,17 @@ package br.com.cod3r.cm.visao;
 import br.com.cod3r.cm.modelo.Tabuleiro;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PainelTabuleiro extends JPanel {
 
     public PainelTabuleiro(Tabuleiro tabuleiro) {
+        setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
 
+        tabuleiro.paraCada(c -> add(new BotaoCampo(c)));
+
+        tabuleiro.registrarObservador(e -> {
+            // TODO mostrar resultado para usuário
+        });
     }
 }
